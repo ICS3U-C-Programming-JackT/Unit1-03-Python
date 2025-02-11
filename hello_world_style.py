7064ef4e75102092e340f92a140ddf90d27368ce
Jack Turcotte <jack.turcotte@stu.ocsb.ca>
2025-02-11 17:35:27 +0000

    Completed program

---
+++ hello_world_style.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Created By: Jack Turcotte
+# February 2nd 2025
+# This program displays "Hello world" with proper style
+
+
+def main():
+    print("This is my hello world program using proper style!")
+    print("This is my first function in python")
+
+
+if __name__ == "__main__":
+    main()
